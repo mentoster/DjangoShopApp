03201db6cf462c6aeea76156952ec825a564a5dc
Dmitriy <Dimamakarov1941@yandex.ru>
2021-02-14 13:04:12 +0300

    update htmls, add start setting docker

--- frobshop/frobshop/settings.py
+++ frobshop/frobshop/settings.py
@@ -141,18 +141,18 @@ WSGI_APPLICATION = 'frobshop.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django-db',
-        'USER': 'postgresql',
-        'PASSWORD': 'HackDb',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        # 'ATOMIC_REQUESTS': True,
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
