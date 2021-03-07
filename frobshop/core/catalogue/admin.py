b00cb96e4e56e8cd4d952f0ebc45c67125b0fa5f
Dmitriy <Dimamakarov1941@yandex.ru>
2021-03-07 17:23:22 +0300

    import export work

--- frobshop/core/catalogue/admin.py
+++ frobshop/core/catalogue/admin.py
@@ -1,17 +1,9 @@
 from oscar.apps.catalogue.admin import *  # noqa
-from .models import Product
-from import_export import resources
 from import_export.admin import ImportExportModelAdmin
+from django.contrib import admin
+from .models import Person
 
 
-class ProductResource(resources.Resource):
-    class Meta:
-        model = Product
-
-
-class ProductAdmin(ImportExportModelAdmin):
-    resource_class = ProductResource
-
-
-admin.site.unregister(Product)
-admin.site.register(Product, ProductAdmin)
+@admin.register(Person)
+class PersonAdmin(ImportExportModelAdmin):
+    pass
--- frobshop/core/catalogue/models.py
+++ frobshop/core/catalogue/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 from oscar.apps.catalogue.abstract_models import AbstractProduct
 
-class Product(AbstractProduct):
-    video_url = models.URLField()
+
+class Person(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField(blank=True)
+    birth_date = models.DateField()
+    location = models.CharField(max_length=100, blank=True)
+
 
 from oscar.apps.catalogue.models import *  # noqa isort:skip
