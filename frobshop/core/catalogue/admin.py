859059ec0f086a02c67a365c782b8e085c61cb51
Dmitriy <Dimamakarov1941@yandex.ru>
2021-03-07 13:49:21 +0300

    add export import to admin page

--- frobshop/core/catalogue/admin.py
+++ frobshop/core/catalogue/admin.py
@@ -1,8 +1,17 @@
 from oscar.apps.catalogue.admin import *  # noqa
 from .models import Product
 from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 
 class ProductResource(resources.Resource):
     class Meta:
         model = Product
+
+
+class ProductAdmin(ImportExportModelAdmin):
+    resource_class = ProductResource
+
+
+admin.site.unregister(Product)
+admin.site.register(Product, ProductAdmin)
