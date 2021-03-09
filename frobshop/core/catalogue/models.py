a8baf14bf5509d55eb594be7ed0507d34980c1c1
Dmitriy Makkarov <Dimamakarov1941@yandex.ru>
2021-03-09 14:01:23 +0300

    add new paarametrs

--- frobshop/core/catalogue/models.py
+++ frobshop/core/catalogue/models.py
@@ -4,9 +4,13 @@ from oscar.apps.catalogue.abstract_models import AbstractProduct
 
 
 class Product(AbstractProduct):
+    article = models.TextField(max_length=30)
+    series = models.TextField(max_length=20)
+    seo_name_for_site = models.TextField(max_length=50)
+    category = models.TextField(max_length=20)
     barcode = models.TextField(max_length=13)
-    image_url = models.URLField()
     attribute = models.TextField(max_length=10)
+    image_url = models.URLField()
 
 
 from oscar.apps.catalogue.models import *  # noqa isort:skip
