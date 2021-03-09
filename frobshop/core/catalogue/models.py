93623d4df516b9b81f338d534e175f59ba6889d8
Dmitriy Makkarov <Dimamakarov1941@yandex.ru>
2021-03-09 13:40:35 +0300

    add attribute

--- frobshop/core/catalogue/models.py
+++ frobshop/core/catalogue/models.py
@@ -6,6 +6,7 @@ from oscar.apps.catalogue.abstract_models import AbstractProduct
 class Product(AbstractProduct):
     barcode = models.TextField(max_length=13)
     image_url = models.URLField()
+    attribute = models.TextField(max_length=10)
 
 
 from oscar.apps.catalogue.models import *  # noqa isort:skip
